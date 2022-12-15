d42375b8817137ae8e50720bdeaa825687879a99
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2022-12-15 12:36:42 +0100

    Booking table creation, controller and Model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+// Booking routes
+
+
+// Route::resource('bookings', BookingController::class);
+Route::get('/event/{event_id}/booking/', [BookingController::class, 'index']);
+Route::post('/event/{event_id}/booking/', [BookingController::class, 'create']);
+Route::delete('/event/{event_id}/booking/', [BookingController::class, 'destroy']);
+
 
 Route::get('/', function () {
     return view('welcome');
