84d6d310e40e05933bc8856e65bd752eba27141f
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2022-12-15 14:37:48 +0000

    Added event endpoints.

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $events = Event::all();
+        return response($events, 200)->header('Content-Type', 'text/plain');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $event = new Event([
+            'uuid' => Str::uuid(),
+            'title' => $request->title,
+            'description' => $request->description,
+            'duration' => $request->duration,
+            'hostname' => $request->hostname
+        ]);
+
+        $event->save();
+        return response(200)->header('Content-Type', 'text/plain');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $event = Event::where('id', $id)->get();
+        return response($event, 200)->header('Content-Type', 'text/plain');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+
+        $event = Event::where('id', $id);
+        $event->delete();
+    }
+}
