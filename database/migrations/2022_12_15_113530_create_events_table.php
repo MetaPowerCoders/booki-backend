c26da42b1f607d973456545dd151dd938de858f0
Lorena Gil <lorenagil@fb.com>
2022-12-15 11:38:51 +0000

    Added Event model and migration.

---
+++ database/migrations/2022_12_15_113530_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('title');
+            $table->longText('description');
+            $table->integer('duration');
+            $table->string('hostname');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
